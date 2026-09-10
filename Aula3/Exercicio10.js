91ac963228b881e831efceab6d45b8be4183b94e
Sidnei Baldo Junior <4501496052@estudante.cedupguaramirim.com.br>
2026-09-10 20:52:27 -0300

    Aula5

--- Aula3/Exercicio10.js
+++ Aula3/Exercicio10.js
@@ -6,37 +6,35 @@ const conexao = mysql.createConnection ({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "biblioteca_atv3"
+    database: "laboratorio_avaliacao"
 })
-
-// trocar funçoes
 
 function menu() {
 
     console.log("\n ===== MENU =====")
-    console.log("1 - Cadastrar livro")
-    console.log("2 - Excluir livro")
-    console.log("3 - Listar livro")
-    console.log("4 - Atualizar livro")
+    console.log("1 - Cadastrar computador")
+    console.log("2 - Excluir computador")
+    console.log("3 - Listar computador")
+    console.log("4 - Atualizar computador")
     console.log("0 - Sair")
 
     const opcao = readline.questionInt("Digite a opção: ")
 
     if (opcao === 1) {
 
-        cadastrarLivro()
+        cadastrarComputador()
 
     } else if (opcao === 2 ) {
 
-        deletarLivro()
+        deletarComputador()
 
     } else if (opcao === 3) {
 
-        listarLivro()
+        listarComputador()
     
     } else if (opcao === 4) {
 
-        atualizarLivro()
+        atualizarComputador()
 
     } else if (opcao === 0 ) {
 
@@ -50,76 +48,97 @@ function menu() {
     }
 }
 
-function atualizarLivro() {
+function atualizarComputador() {
+    const id = readline.questionInt("Digite o ID do computador: ");
+    const sql = "select * from computadores where id = ?";
 
-    const titulo = readline.question("Digite o título do livro: ")
-    const autor = readline.question("Digite o autor do livro: ")
-
-    const id = readline.questionInt("Digite o ID do livro: ")
-
-    const update = "update livros set titulo = ?, autor = ? where id = ?"
-
-    conexao.query(update[titulo, autor, id], function(erro, resultado) {
-
+    conexao.query(sql, [id], function(erro, resultado) {
         if (erro) {
+            console.log("Erro ao buscar computador:");
+            console.log(erro);
+            menu(); 
+        } 
+        else if (resultado.length === 0) {
+            console.log("Computador não encontrado!");
+            menu(); 
+        } 
+        else {
+            console.log("\n--- STATUS ATUAIS ---");
+            resultado.forEach(function (computador) {
+                console.log(
+                    "Localização: " + computador.localizacao + " | " +
+                    "Responsável: " + computador.responsavel + " | " +
+                    "Status: " + computador.status
+                );
+            });
+            console.log("----------------------\n");
 
-            console.log("Erro ao atualizar livro.")
-            console.log(erro)
+            const localizacao = readline.question("Digite a nova localização: ");
+            const responsavel = readline.question("Digite o novo responsável: ");
+            const status = readline.question("Digite o novo status: ");
 
-        } else if (resultado.affectedRows === 0) {
+            const update = "update computadores set localizacao = ?, responsavel = ?, status = ? where id = ?";
 
-            console.log("Livro não encontrado")
-
-        } else {
-
-            console.log("Livro atualizado com sucesso")
-
+            conexao.query(update, [localizacao, responsavel, status, id], function(erro, resultadoUpdate) {
+                if (erro) {
+                    console.log("Erro ao atualizar Computador.");
+                    console.log(erro);
+                } else {
+                    console.log("Computador atualizado com sucesso!");
+                }
+                
+               
+                menu();
+            });
         }
-    })
-
-    menu()
+    });
 }
 
-function cadastrarLivro() {
+function cadastrarComputador() {
 
-    const titulo = readline.question("Digite o título do livro: ")
-    const autor = readline.question("Digite o autor do livro: ")
+    const patrimonio = readline.question("Digite o patrimônio: ")
+    const localizacao = readline.question("Digite a localização: ")
+    const responsavel = readline.question("Digite o responsável: ")
+    const status = readline.question("Digite o status: ")
 
-    const inserir = "insert into livros (titulo, autor) values (?, ?)"
+    const inserir = "insert into computadores (patrimonio, localizacao, responsavel, status) values (?, ?, ?, ?)"
 
-    conexao.query(inserir, [titulo, autor], function(erro){
+    conexao.query(inserir, [patrimonio, localizacao, responsavel, status], function(erro){
         
         if (erro) {
 
-            console.log("Erro ao cadastrar livro.")
+            console.log("Erro ao cadastrar computador.")
             console.log(erro)
 
         }  else {
 
-            console.log("Livro cadastrado com sucesso.")
+            console.log("Computador cadastrado com sucesso.")
             menu()
         }
     })
 }
 
-function listarLivro() {
+function listarComputador() {
     
-    const sql = "select titulo, autor from livros"
+    const sql = "select patrimonio, localizacao, responsavel, status from computadores"
 
-    conexao.query(sql, function(erro, livros) {
+    conexao.query(sql, function(erro, computadores) {
 
         if (erro) {
 
-            console.log("Erro ao listar livros.")
+            console.log("Erro ao listar os computadores.")
             console.log(erro)
 
         }  else {
 
-            console.log("\n --- LIVROS ---")
-            livros.forEach( function (livros) {
-                console.log(
-                    livros.titulo + " - " +
-                    livros.autor
+            console.log("\n --- COMPUTADORES ---")
+            computadores.forEach( function (computadores) {
+                    console.log(
+                    computadores.id + " - " +
+                    computadores.patrimonio + " - " +
+                    computadores.localizacao + " - " +
+                    computadores.responsavel + " - " +
+                    computadores.status
                 )
             })
         }
@@ -128,32 +147,75 @@ function listarLivro() {
     })
 }
 
-function deletarLivro() {
+function deletarComputador() {
 
-    const id = readline.question("Digite o ID do livro: ")
+    const id = readline.question("Digite o ID do computador: ")
 
-    const deletar = "delete from livros where id = ?"
+    
 
-    conexao.query(deletar, [id], function(erro, resultado) {
-
-        if (erro) {
-
-            console.log("Erro ao deletar livro.")
-            console.log(erro)
-
-        }  else if (resultado.affectedRows === 0 ) {
-
-            console.log("Produto não encontrado")
-
-        }   else {
-
-            console.log("Produto deletado com sucesso.")
-
-        }
-
-        menu()
-
-    })
+    const consultar = "SELECT * FROM computadores WHERE id = ?"
+    
+        conexao.query(consultar, [id], function(erro, resultado) {
+    
+            if (erro) {
+    
+                console.log("Erro ao consultar computador")
+                console.log(erro)
+                menu()
+                return
+    
+            }
+    
+            if (resultado.length === 0) {
+    
+                console.log("Computador não encontrado")
+                menu()
+                return
+    
+            }
+    
+            console.log("\nComputador encontrado:")
+            console.log("Patrimônio:", resultado[0].patrimonio)
+            console.log("Localização:", resultado[0].localizacao)
+            console.log("Responsável:", resultado[0].responsavel)
+            console.log("Status:", resultado[0].status)
+    
+            const resposta = readline.question("Deseja excluir? (S/N): ").toLowerCase()
+    
+            if (resposta === "s") {
+    
+                const deletar = "DELETE FROM computadores WHERE id = ?"
+    
+                conexao.query(deletar, [id], function(erro, resultado) {
+    
+                    if (erro) {
+    
+                        console.log("Erro ao deletar computador")
+                        console.log(erro)
+    
+                    } else {
+    
+                        console.log("Computador deletado com sucesso.")
+    
+                    }
+    
+                    menu()
+    
+                })
+    
+            } else if (resposta === "n") {
+    
+                console.log("Exclusão cancelada.")
+                menu()
+    
+            } else {
+    
+                console.log("Opção inválida.")
+                menu()
+    
+            }
+    
+        })
 }
 
 menu()
